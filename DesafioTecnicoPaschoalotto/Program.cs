8a2e92f3f01a6e815452fd3dcaa0b420a726b88c
Luciano <lucianoliv2@hotmail.com>
2024-03-11 19:04:08 -0300

    Acessar e digitar as palavras

--- DesafioTecnicoPaschoalotto/Program.cs
+++ DesafioTecnicoPaschoalotto/Program.cs
@@ -1,14 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DesafioTecnicoPaschoalotto.objetos;
+using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
 
-program.program.inicio();
-
-namespace program
+automacao.Program.ExecutarAutomacao();
+namespace automacao
 {
-    public static class program
+    public static class Program
     {
-        public static void inicio()
+        public static void ExecutarAutomacao()
         {
-            Console.WriteLine("Hello, World!");
+            var webDriver = new EdgeDriver();
+            webDriver.Navigate().GoToUrl("https://10fastfingers.com/typing-test/portuguese");
+            var BtndenyCookie = webDriver.FindElement(By.XPath("/html/body/div[1]/div/div[4]/div[1]/div[2]/button[1]"));
+            BtndenyCookie.Click();
+            var txtBoxInserirPalavras = webDriver.FindElement(By.Id("inputfield"));
+
+            var htmlContend = webDriver.PageSource;
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(htmlContend);
+
+            var palavras = htmlDocument.GetElementbyId("words").ChildNodes;
+
+            foreach(var palavra in palavras)
+            {
+                var letras = palavra.InnerText;
+                foreach(var letra in letras)
+                {                   
+                    txtBoxInserirPalavras.SendKeys($"{letra}");                   
+                }
+                txtBoxInserirPalavras.SendKeys(" ");
+            }
+            
+            var resultado = new Resultado();            
         }
     }      
 } 
---
+++ DesafioTecnicoPaschoalotto/objetos/Resultado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioTecnicoPaschoalotto.objetos
+{
+    internal class Resultado
+    {
+        public Resultado()
+        {
+            this.wordsPerMinute = "";
+            this.keyStrokes = "";
+            this.correctWords = "";
+            this.WrongWords = ""; 
+        }
+        public string wordsPerMinute { get; set; }
+        public string keyStrokes { get; set; }
+        public string correctWords { get; set; }
+        public string WrongWords { get ; set; }
+    }
+}
