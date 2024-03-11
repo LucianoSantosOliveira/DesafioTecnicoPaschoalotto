70b758e97c4a9a85632ab852339cc506b3d4afc4
Luciano <lucianoliv2@hotmail.com>
2024-03-11 20:29:37 -0300

    ajuste

--- DesafioTecnicoPaschoalotto/Program.cs
+++ DesafioTecnicoPaschoalotto/Program.cs
@@ -10,29 +10,42 @@ namespace automacao
     {
         public static void ExecutarAutomacao()
         {
-            var webDriver = new EdgeDriver();
-            webDriver.Navigate().GoToUrl("https://10fastfingers.com/typing-test/portuguese");
-            var BtndenyCookie = webDriver.FindElement(By.XPath("/html/body/div[1]/div/div[4]/div[1]/div[2]/button[1]"));
-            BtndenyCookie.Click();
-            var txtBoxInserirPalavras = webDriver.FindElement(By.Id("inputfield"));
-
-            var htmlContend = webDriver.PageSource;
-
-            var htmlDocument = new HtmlDocument();
-            htmlDocument.LoadHtml(htmlContend);
-
-            var palavras = htmlDocument.GetElementbyId("words").ChildNodes;
-
-            foreach(var palavra in palavras)
+            try
             {
-                var letras = palavra.InnerText;
-                foreach(var letra in letras)
-                {                   
-                    txtBoxInserirPalavras.SendKeys($"{letra}");                   
+                var webDriver = new EdgeDriver();
+                webDriver.Navigate().GoToUrl("https://10fastfingers.com/typing-test/portuguese");
+                var BtndenyCookie = webDriver.FindElement(By.XPath("/html/body/div[1]/div/div[4]/div[1]/div[2]/button[1]"));
+                BtndenyCookie.Click();
+                var txtBoxInserirPalavras = webDriver.FindElement(By.Id("inputfield"));
+
+                var htmlContend = webDriver.PageSource;
+
+                var htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(htmlContend);
+
+                var palavras = htmlDocument.GetElementbyId("words").ChildNodes;
+
+                foreach (var palavra in palavras)
+                {
+                    var letras = palavra.InnerText;
+                    foreach (var letra in letras)
+                    {
+                        txtBoxInserirPalavras.SendKeys($"{letra}");
+                    }
+                    txtBoxInserirPalavras.SendKeys(" ");
                 }
-                txtBoxInserirPalavras.SendKeys(" ");
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro, execute a aplicação novamente {ex.Message}");
+            }
+            
             Environment.Exit(0);       
+        }
+
+        public static void exibirResultados()
+        {
+
         }
     }      
 } 
--- DesafioTecnicoPaschoalotto/Utilitarios/BancoDeDados.cs
+++ DesafioTecnicoPaschoalotto/Utilitarios/BancoDeDados.cs
@@ -5,13 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace DesafioTecnicoPaschoalotto.Utilitarios
 {
     internal class BancoDeDados
     {
+        private string dataBaseAddress = "Server=tcp:desafiopaschoalotto.database.windows.net,1433;Initial Catalog=DesafioPaschoalotto;Persist Security Info=False;User ID=Luciano;Password={Pontiacgto123@};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public void InserirResultadosNoBD(Resultado resultado)
         {
-
+            
         }
     }
 }
