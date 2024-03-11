a3c43fe9ae5f29612f9a4ce0a2e75dc59493ab4e
Luciano <lucianoliv2@hotmail.com>
2024-03-11 17:33:22 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioTecnicoPaschoalotto/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+program.program.inicio();
+
+namespace program
+{
+    public static class program
+    {
+        public static void inicio()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }      
+} 
+
